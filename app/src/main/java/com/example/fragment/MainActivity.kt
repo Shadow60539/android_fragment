97805dffbff8338396cb686bc8ccf0f4eccd82e4
Sanjeev Madhav <sduddela@tataunistore.com>
2022-07-13 16:40:01 +0530

    add backstack

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -1,22 +1,48 @@
 package com.example.fragment
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
+    private val fm: FragmentManager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        addFragment()
+        addFirstFragment()
+        fm.addOnBackStackChangedListener() {
+            findViewById<TextView>(R.id.tvBackStackCount).text = fm.backStackEntryCount.toString()
+        }
     }
 
-    private fun addFragment() {
-        val fm: FragmentManager = supportFragmentManager
+    private fun addFirstFragment() {
         val ft: FragmentTransaction = fm.beginTransaction()
         val firstFragment = FirstFragment()
-        ft.add( R.id.fragment,firstFragment)
+        ft.add(R.id.fragment, firstFragment)
         ft.commit()
+    }
+
+    fun replaceFragment(v: View) {
+        val ft: FragmentTransaction = fm.beginTransaction()
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+        val currentFragment = fm.findFragmentById(R.id.fragment)!!
+        if (currentFragment is FirstFragment) {
+            ft.replace(R.id.fragment, secondFragment)
+        } else {
+            ft.replace(R.id.fragment, firstFragment)
+        }
+        ft.addToBackStack(null)
+        ft.commit()
+    }
+
+    override fun onBackPressed() {
+        findViewById<TextView>(R.id.tvBackStackCount).text = fm.backStackEntryCount.toString()
+        super.onBackPressed()
     }
 }
