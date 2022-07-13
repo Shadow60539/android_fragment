81135f81e8d7c47a6bafe9f571a9114172b31a15
Sanjeev Madhav <sduddela@tataunistore.com>
2022-07-13 13:58:22 +0530

    add fragment dynamically

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.fragment
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addFragment()
+    }
+
+    private fun addFragment() {
+        val fm: FragmentManager = supportFragmentManager
+        val ft: FragmentTransaction = fm.beginTransaction()
+        val firstFragment = FirstFragment()
+        ft.add( R.id.fragment,firstFragment)
+        ft.commit()
     }
 }
