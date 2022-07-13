1602db6bfaa0052c8704cd603724921f9a745192
Sanjeev Madhav <sduddela@tataunistore.com>
2022-07-13 17:20:13 +0530

    handle orientation state

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -4,26 +4,33 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
+    private val COUNT_KEY = "COUNT_KEY"
+    private val FRAGMENT_KEY = "FRAGMENT_KEY"
+
+    private lateinit var textViewBackStackCounter: TextView
 
     private val fm: FragmentManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        addFirstFragment()
+        textViewBackStackCounter = findViewById<TextView>(R.id.tvBackStackCount)
+
+        showFragment(FirstFragment())
         fm.addOnBackStackChangedListener() {
-            findViewById<TextView>(R.id.tvBackStackCount).text = fm.backStackEntryCount.toString()
+            textViewBackStackCounter.text = fm.backStackEntryCount.toString()
         }
     }
 
-    private fun addFirstFragment() {
+    private fun showFragment(fragment: Fragment) {
         val ft: FragmentTransaction = fm.beginTransaction()
-        val firstFragment = FirstFragment()
-        ft.add(R.id.fragment, firstFragment)
+        ft.replace(R.id.fragment, fragment)
         ft.commit()
     }
 
@@ -42,7 +49,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        findViewById<TextView>(R.id.tvBackStackCount).text = fm.backStackEntryCount.toString()
+        textViewBackStackCounter.text = fm.backStackEntryCount.toString()
         super.onBackPressed()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        var currentFragment: Fragment
+        var count: String
+
+        savedInstanceState.apply {
+            currentFragment = if (getBoolean(FRAGMENT_KEY)) FirstFragment() else SecondFragment()
+            count = getString(COUNT_KEY)!!
+        }
+        textViewBackStackCounter.text = count
+        showFragment(currentFragment)
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        val isFirst = fm.findFragmentById(R.id.fragment)!! is FirstFragment
+        val count = textViewBackStackCounter.text.toString()
+        outState.apply {
+            putBoolean(FRAGMENT_KEY, isFirst)
+            putString(COUNT_KEY, count)
+        }
+        super.onSaveInstanceState(outState)
     }
 }
